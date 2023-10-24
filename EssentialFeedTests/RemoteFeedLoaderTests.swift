fc4606cb25994a004364db6696547dc34b0e7f59
Maxime ROUX <maxime.roux@ca-ts.fr>
2023-10-24 16:23:07 +0200

    Request data from URL on 'load'

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -1,28 +1,42 @@
 //
-//  RemoteFeedLoaderTests.swift
-//  RemoteFeedLoaderTests
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
 //
 //  Created by ROUX Maxime on 24/10/2023.
 //
 
 import XCTest
+//@testable import EssentialFeed
 
 class RemoteFeedLoader {
-  
+  func load() {
+    HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+  }
 }
 
 class HTTPClient {
+  static let shared = HTTPClient()
+  
+  private init() {}
+  
   var requestedURL: URL?
 }
 
-class RemoteFeedLoaderTests: XCTest {
-  
+class RemoteFeedLoaderTests: XCTestCase {
   
   func test_init_doesNotRequestDataFromURL() {
-    let client = HTTPClient()
+    let client = HTTPClient.shared
     _ = RemoteFeedLoader()
     
     XCTAssertNil(client.requestedURL)
+  }
+  
+  func test_load_requestDataFromURL() {
+    let client = HTTPClient.shared
+    let sut = RemoteFeedLoader()
     
+    sut.load()
+    
+    XCTAssertNotNil(client.requestedURL)
   }
 }
