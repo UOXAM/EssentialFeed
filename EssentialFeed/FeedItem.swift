95e1aa3e8ca0f1f55d972b74044e2a5e865134bb
Maxime ROUX <maxime.roux@ca-ts.fr>
2023-10-24 14:40:37 +0200

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by ROUX Maxime on 24/10/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageUrl: URL
+}
