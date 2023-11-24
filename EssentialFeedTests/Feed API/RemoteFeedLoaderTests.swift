a5b31b426e07b58273e028323d4e5119fe8ed442
Maxime ROUX <r.maxime@yandex.com>
2023-11-24 15:53:13 +0100

    Improve map

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -162,7 +162,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   private class HTTPClientSpy: HTTPClient {
     private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
     var requestedURLs: [URL] {
-      return messages.map { $0.url }
+      return messages.map(\.url)
     }
     
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
